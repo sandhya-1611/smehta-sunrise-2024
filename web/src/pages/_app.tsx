9c2383a71782e00eb736da8273d1a1065cbcb98b
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-08-06 01:34:19 +0530

    task completed

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import 'antd/dist/reset.css';
+// import 'antd/dist/antd.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
